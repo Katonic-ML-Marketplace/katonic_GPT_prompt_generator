6b71a9b01c7182765ba985e8fdc799bdf3b4def9
kolhe-samiksha <samiksha-kohle@katonic.ai>
2023-02-10 13:18:57 +0000

    "Modified About and Main_App"

--- Katonic_app.py
+++ Katonic_app.py
@@ -15,9 +15,13 @@ with st.sidebar:
 
 st.image('katonic_logo.png')
 if selected == "About":
-    st.title("Katonic-GPT Prompt Generator App")
-    st.write("""This Katonic_GPT Prompt generator generates ChatGPT prompts, it's based on a BART model trained on [this dataset](https://huggingface.co/datasets/fka/awesome-chatgpt-prompts). 📓 You Simply enter a persona that you want and it'll generate the Prompt🤩
-    """)
+    st.title("Katonic-GPT Prompt Generator App🚀")
+    st.markdown("""This **Katonic_GPT Prompt generator** generates ChatGPT prompts, it's based on a BART model trained on [awesome-gpt-prompts-dataset](https://huggingface.co/datasets/fka/awesome-chatgpt-prompts). 📓 
+    The Prompts will make our and ChatGPT life easier, so to make full-proof and efficient use of ChatGPT, Use this App to generate the Prompts""")
+    st.markdown("""You can Follow these steps to generate the prompts👇:""")
+    st.markdown("1. Enter the Persona that you want e.g. Photographer,developer 👩‍💻👩🏻‍🚀🧝🏻‍♀️👨🏻‍🏫👨🏻‍⚖️👨🏻‍🎨👨🏻‍🎓")
+    st.markdown("2. Enter on the submit button ➖ It'll generate prompts.")
+    st.markdown("3. Copy the Prompts and paste it to the ChatGPT [here](https://chat.openai.com/).")
 
 if selected == "Katonic_GPT_Prompt Generator":
 
@@ -28,6 +32,8 @@ if selected == "Katonic_GPT_Prompt Generator":
         generated_ids = model.generate(batch["input_ids"], max_new_tokens=150)
         output = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
         st.code(output[0])
+
+        st.markdown("""You can Copy the Prompts👆 and just paste it in ChatGPT to get the desired result🤩""")
 
 
 
